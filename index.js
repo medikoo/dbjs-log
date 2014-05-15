c7f935e4c14cf11bab448d92223b54b4b8bfdcad
Mariusz Nowak <mariusz@medikoo.com>
2014-05-15 11:16:19 +0200

    Do not auto create log directory

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var database      = require('dbjs/valid-dbjs')
   , resolve       = require('path').resolve
-  , mkdir         = require('fs2/mkdir')
   , writeSnapshot = require('./lib/write-snapshot')
   , writeLog      = require('./lib/write-log');
 
@@ -10,9 +9,7 @@ module.exports = function (db, logPath) {
 	var dateString = (new Date()).toISOString();
 	database(db);
 	logPath = resolve(String(logPath));
-	return mkdir(logPath, { intermediate: true })(function () {
-		return writeSnapshot(db, resolve(logPath, dateString + '-init.db.snapshot'));
-	})(function () {
+	return writeSnapshot(db, resolve(logPath, dateString + '-init.db.snapshot'))(function () {
 		return writeLog(db, resolve(logPath, dateString + '.db.log'));
 	});
 };
