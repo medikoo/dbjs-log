f485f85210423a31b7eefc66f689a3b9c1025d3a
Mariusz Nowak <mariusz@medikoo.com>
2014-05-19 10:05:03 +0200

    Do not use ':' in filenames

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 var database      = require('dbjs/valid-dbjs')
   , resolve       = require('path').resolve
   , writeSnapshot = require('./lib/write-snapshot')
-  , writeLog      = require('./lib/write-log');
+  , writeLog      = require('./lib/write-log')
+  , re            = /:/g;
 
 module.exports = function (db, logPath) {
-	var dateString = (new Date()).toISOString();
+	var dateString = (new Date()).toISOString().replace(re, 'x');
 	database(db);
 	logPath = resolve(String(logPath));
 	return writeSnapshot(db, resolve(logPath, dateString + '-init.db.snapshot'))(function () {
--- test/lib/resolve-log.js
+++ test/lib/resolve-log.js
@@ -8,8 +8,8 @@ module.exports = {
 		t(path).done(function (data) {
 			a(data.snapshot.length, 8, "Snapshot length");
 			a(data.log.length, 4, "Log length");
-			a(data.snapshotFilename, '2014-05-14T15:11:56.661Z-init.db.snapshot', "Snapshot filename");
-			a(data.logFilename, '2014-05-14T15:11:56.661Z.db.log', "Log filename");
+			a(data.snapshotFilename, '2014-05-14T15x11x56.661Z-init.db.snapshot', "Snapshot filename");
+			a(data.logFilename, '2014-05-14T15x11x56.661Z.db.log', "Log filename");
 			d();
 		}, d);
 	},
@@ -17,8 +17,8 @@ module.exports = {
 		t(path, { at: 1400080290283378 }).done(function (data) {
 			a(data.snapshot.length, 6, "Snapshot length");
 			a(data.log.length, 2, "Log length");
-			a(data.snapshotFilename, '2014-05-14T15:11:29.762Z-init.db.snapshot', "Snapshot filename");
-			a(data.logFilename, '2014-05-14T15:11:29.762Z.db.log', "Log filename");
+			a(data.snapshotFilename, '2014-05-14T15x11x29.762Z-init.db.snapshot', "Snapshot filename");
+			a(data.logFilename, '2014-05-14T15x11x29.762Z.db.log', "Log filename");
 			d();
 		}, d);
 	}
